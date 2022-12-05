d6039aba5f3c2f9392e1580586ff536aa2a26cf8
Llugdar Emilio Said <93954241+EmilioSaidLlugdar@users.noreply.github.com>
2022-12-05 10:36:05 -0300

    modificacion

--- auto_text_effect/script.js
+++ auto_text_effect/script.js
@@ -7,7 +7,11 @@ let speed=300/speedE1.value
 whriteTExt()
 
 function whriteTExt(){
+    //innerText establece o devuelve el contenido de texto como 
+    //texto sin formato del nodo especificado y todos sus descendientes
     textE1.innerText=text.slice(0,idx)
+    //slice() El método slice() devuelve una copia de una parte del array 
+    //dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará.
     idx++
 
     if(idx>text.length){
@@ -15,6 +19,13 @@ function whriteTExt(){
     }
 
     setTimeout(whriteTExt,speed)
+    //El método setTimeout() de JavaScript es un método incorporado que permite temporizar la 
+    //ejecución de una determinada función . Es necesario pasar la cantidad de tiempo a esperar en milisegundos
+    //lo que significa que para esperar un segundo, es necesario pasar mil milisegundos 
 }
 
-speedE1.addEventListener('input',(e)=> speed =300/e.target.value)
+speedE1.addEventListener('input',(e)=> speed =300/ e.target.value)
+
+/*El método addEventlistener, es un escuchador que indica al navegador que este atento 
+a la interacción del usuario. La ventaja es que se escribe totalmente en el JS, 
+sin necesidad de tocar el HTML. */
